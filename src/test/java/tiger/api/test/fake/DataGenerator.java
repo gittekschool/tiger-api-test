85b72d5d4292a402bc67265c71c9fd70882e7744
Mohammad Shokriyan <mohammad@tekschool.us>
2022-07-25 20:25:13 -0400

    This is my first Commit this can be any message

---
+++ src/test/java/tiger/api/test/fake/DataGenerator.java
@@ -0,0 +1,40 @@
+package tiger.api.test.fake;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.github.javafaker.Faker;
+
+public class DataGenerator {
+	
+	public static String getEmail() {
+		Faker faker = new Faker(); 
+		return faker.name().firstName() 
+				+ faker.name().lastName() 
+				+ "@gmail.com";
+	}
+	
+	public static String getPhoneNumber() {
+		Faker faker = new Faker(); 
+		return faker.phoneNumber().cellPhone();
+	}
+	
+	public static String getFirstName() {
+		Faker faker = new Faker(); 
+		return faker.name().firstName(); 
+	}
+	
+	public static String getLastName() {
+		Faker faker = new Faker(); 
+		return faker.name().lastName(); 
+	}
+	
+	public static String getDob() {
+		Faker faker = new Faker(); 
+		Date date = faker.date().birthday();
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		return format.format(date);
+	}
+
+
+}
